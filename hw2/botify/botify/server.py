8b9c449505955cbc3910371e24cff78c6d86507f
Шпилева Анастасия <62290933+banka-lecho@users.noreply.github.com>
2024-05-22 12:52:58 +0300

    add hw2

---
+++ hw2/botify/botify/server.py
@@ -0,0 +1,131 @@
+import json
+import logging
+import time
+from dataclasses import asdict
+from datetime import datetime
+
+from flask import Flask
+from flask_redis import Redis
+from flask_restful import Resource, Api, abort, reqparse
+from gevent.pywsgi import WSGIServer
+
+from recommenders.contextual import Contextual
+from recommenders.Indexed import Indexed
+from data import DataLogger, Datum
+from experiment import Experiments, Treatment
+from recommenders.random import Random
+from recommenders.toppop import TopPop
+from recommenders.my_recommender import MyRecommender
+from track import Catalog
+
+root = logging.getLogger()
+root.setLevel("INFO")
+
+app = Flask(__name__)
+app.config.from_file("config.json", load=json.load)
+api = Api(app)
+
+tracks_redis = Redis(app, config_prefix="REDIS_TRACKS")
+artists_redis = Redis(app, config_prefix="REDIS_ARTIST")
+recommendations_dssm = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_DSSM")
+recommendations_gcf = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_GCF")
+
+used = Redis(app, config_prefix="REDIS_USED")
+combined_tracks_set = Redis(app, config_prefix="REDIS_COMBINED_TRACKS_SET")
+liked_tracks = Redis(app, config_prefix="LIKED_TRACKS")
+data_logger = DataLogger(app)
+
+catalog = Catalog(app).load(app.config["TRACKS_CATALOG"])
+catalog.upload_tracks(tracks_redis.connection)
+catalog.upload_artists(artists_redis.connection)
+catalog.upload_recommendations(
+    recommendations_dssm.connection, "RECOMMENDATIONS_DSSM_FILE_PATH"
+)
+catalog.upload_recommendations(
+    recommendations_gcf, "RECOMMENDATIONS_GCF_FILE_PATH"
+)
+
+top_tracks = TopPop.load_from_json(app.config["TOP_TRACKS"])
+
+parser = reqparse.RequestParser()
+parser.add_argument("track", type=int, location="json", required=True)
+parser.add_argument("time", type=float, location="json", required=True)
+
+
+class Hello(Resource):
+    def get(self):
+        return {
+            "status": "alive",
+            "message": "welcome to botify, the best toy music recommender",
+        }
+
+
+class Track(Resource):
+    def get(self, track: int):
+        data = tracks_redis.connection.get(track)
+        if data is not None:
+            return asdict(catalog.from_bytes(data))
+        else:
+            abort(404, description="Track not found")
+
+
+class NextTrack(Resource):
+    def post(self, user: int):
+        start = time.time()
+
+        args = parser.parse_args()
+        treatment = Experiments.AA.assign(user)
+
+        if treatment == Treatment.T1:
+            recommender = MyRecommender(recommendations_dssm.connection,
+                                        recommendations_gcf.connection,
+                                        catalog,
+                                        Random(recommendations_dssm.connection),
+                                        used.connection,
+                                        combined_tracks_set.connection)
+        else:
+            recommender = Indexed(recommendations_dssm.connection, catalog, Random(tracks_redis.connection))
+
+        recommendation = recommender.recommend_next(user, args.track, args.time)
+
+        data_logger.log(
+            "next",
+            Datum(
+                int(datetime.now().timestamp() * 1000),
+                user,
+                args.track,
+                args.time,
+                time.time() - start,
+                recommendation,
+            ),
+        )
+        return {"user": user, "track": recommendation}
+
+
+class LastTrack(Resource):
+    def post(self, user: int):
+        start = time.time()
+        args = parser.parse_args()
+        data_logger.log(
+            "last",
+            Datum(
+                int(datetime.now().timestamp() * 1000),
+                user,
+                args.track,
+                args.time,
+                time.time() - start,
+            ),
+        )
+        return {"user": user}
+
+
+api.add_resource(Hello, "/")
+api.add_resource(Track, "/track/<int:track>")
+api.add_resource(NextTrack, "/next/<int:user>")
+api.add_resource(LastTrack, "/last/<int:user>")
+
+app.logger.info(f"Botify service stared")
+
+if __name__ == "__main__":
+    http_server = WSGIServer(("", 5002), app)
+    http_server.serve_forever()
